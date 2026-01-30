b97aa6a1b6956f613514b4501a49c2d7c23f71a0
xModern <xmodern@dev.com>
2026-01-30 22:32:32 +0500

    Release v1.7.14: Performance CPU: fix temp selection (median cpu zones) + debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.taskmanager"
         minSdk = 24
         targetSdk = 34
-        versionCode = 21
-        versionName = "1.7.13"
+        versionCode = 22
+        versionName = "1.7.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
