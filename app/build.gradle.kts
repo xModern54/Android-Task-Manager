99edc34c25fd2b560ba2a72964be13e804862d1a
xModern <xmodern@xModerns-MacBook-Air.local>
2026-01-30 21:12:12 +0500

    Release v1.7.8: Performance: reduce selector outline thickness

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.taskmanager"
         minSdk = 24
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.7.7"
+        versionCode = 16
+        versionName = "1.7.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
